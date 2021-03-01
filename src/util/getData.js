da2b57a09a8e2e860a4fcb8f48127e0175431bd7
Paul Barnette <paul.barnette@gmail.com>
2021-03-02 09:53:43 +1100

    Added the getData and testing for getData

---
+++ src/util/getData.js
@@ -0,0 +1,21 @@
+/**
+ * getData - An async function that will return the fetched json data from the service
+ * @param {String} dataUrl  - the url of the data to be returned
+ * @return {Json} The Json from the service
+ */
+
+const getData= async (dataUrl)=>{
+  return fetch(dataUrl
+  ,{
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     }
+  }
+  )
+    .then(function(response){
+      return response.json();
+    });
+}
+
+module.exports.getData = getData;
---
+++ src/util/getData.test.js
@@ -0,0 +1,58 @@
+import { getData } from './getData';
+
+
+describe('getData function testing', () => {
+    let dataReturn = {
+        "size": 25,
+        "updatedAt": "2021-03-01T01:47:15.110Z",
+        "data": [
+            {
+                "quantity": 125000,
+                "startDate": "2021-02-24T23:59:59.999Z",
+                "endDate": "2021-03-11T00:00:00.000Z",
+                "productId": "bbd3f603-7622-43ea-9bda-b98d4c729ec2",
+                "name": "Handcrafted channels",
+                "id": "346bb1a4-2b24-4299-bd2a-3e935e7119dd"
+            }]
+        };
+    
+    beforeEach(() => {    
+        
+      });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+    test('should get data', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () => Promise.resolve(dataReturn),
+            })
+            );
+        const data = await getData("dataURL");
+
+        expect(data).toEqual(dataReturn)
+           
+    
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+    
+      test('should throw a rejection', async () => {
+        global.fetch = jest.fn(() =>
+            Promise.reject({error: "Data unable to be returned."})
+            );
+;
+
+        expect(() =>  getData("dataURL")).rejects.toEqual(
+            expect.objectContaining({
+                "error":"Data unable to be returned." 
+            })
+            )
+           
+    
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+    
+
+
+});

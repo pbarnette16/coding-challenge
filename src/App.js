6cd3985e4b657bb148acacf73487e9389453c619
Paul Barnette <paul.barnette@gmail.com>
2021-03-03 06:36:01 +1100

    Update to the App to display the list, adding in the date-fn functions, converting index.css to index.scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import logo from './components/images/adslot_black.png';
 import './App.scss';
+import {getData}  from './util/getData'
+import {mergeData} from './util/mergeData'
 
 function App() {
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [data, setData] = useState(() => {
+    getData()
+    .then(data => {
+      setIsDataLoaded(true)
+      setData(mergeData(data))
+    })
+  });
+
+  useEffect(() => {
+    console.log('useEffect')
+    console.log(data)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +28,9 @@ function App() {
         <header>
           <h1>Bookings</h1>
         </header>
+        {
+          (!isDataLoaded) ? (<div className="loader">Loading...</div>) : <span></span>
+        }
       </article>
     </div>
   );

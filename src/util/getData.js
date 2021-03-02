39c9061578ddba1327193b114b464ccaa8f0a616
Paul Barnette <paul.barnette@gmail.com>
2021-03-02 12:42:19 +1100

    Added the config object and the getData.js

---
+++ src/util/getData.js
@@ -0,0 +1,47 @@
+const { getMetadata } = require('core-js/fn/reflect')
+const config = require('./src/config/config.json');
+const {getAPI} = require('./src/util/getAPI');
+const differenceInCalendarDays = require('date-fns/differenceInCalendarDays')
+
+/**
+ * getData - A function to return the data from the services and put them in local storage
+ * @param {boolean} forceUpdate - Boolean used to force 
+ * @returns {Object} Object containing the data from the services
+ */
+
+ async function getData(forceUpdate = false) {
+    let bookings, sellers, products = [];
+    const baseUrl = config.baseURL;
+    const endPoints = config.endPoints;
+
+    // if no data in local storage or we want to force an update 
+    // of all the data
+    if(!localStorage.getItem('bookings') || forceUpdate) {
+        
+        [bookings, sellers, products]  = Promise.all([getAPI(baseUrl+endPoints.bookings),
+        getAPI(baseUrl+endPoints.sellers), getAPI(baseUrl+endPoints.products)])
+        
+        // store in local storage
+        localStorage.setItem('bookings', bookings);
+        localStorage.setItem('sellers', sellers);
+        localStorage.setItem('products', products);
+    }
+    else {
+        bookings = localStorage.getItem('bookings');
+        sellers = localStorage.getItem('sellers');
+        products = localStorage.getItem('products');
+
+        // Get the data from local storage, refresh if it is old data
+        if(differenceInCalendarDays(bookings.updatedAt, Date.now()) > 1) {
+            bookings = getAPI(baseUrl + endPoints.bookings)
+            localStorage.setItem('bookings', bookings)
+        }
+    }
+
+    return Promise.resolve({
+        bookings: bookings,
+        sellers: sellers,
+        products: products
+    })
+
+ }
